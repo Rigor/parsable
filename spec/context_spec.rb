c318e6dc43cb1ed629c5b30f6011e8768044b6d0
Hubert Liu <hubert.liu@rigor.com>
2014-02-19 17:50:40 -0500

    fixing spec, adding 2.0.0 for travis

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -13,14 +13,14 @@ describe Parsable::Context do
   describe '#custom_store' do
     it "stores custom variables in the custom object_key" do
       context.custom_store :email, "test@test.com"
-      expect(context.instance_variable_get('@variables')[:custom][:email]).to eql("test@test.com")
+      expect(context.instance_variable_get('@variables')[:custom].email).to eql("test@test.com")
     end
   end
 
   describe '#system_store' do
     it "stores variables as a top level object_key" do
       context.system_store :test_object, 'test_attribute', 'test_value'
-      expect(context.instance_variable_get('@variables')[:test_object][:test_attribute]).to eql("test_value")
+      expect(context.instance_variable_get('@variables')[:test_object].test_attribute).to eql("test_value")
     end
   end
 
